858d128027fbca7c692b4c4c6602060ef19c680c
Stefan Mertl <info@mertl-research.at>
2022-03-11 15:18:05 +0100

    Added the AttribDict class.

--- lib/mss_service/core/util.py
+++ lib/mss_service/core/util.py
@@ -45,6 +45,25 @@ def get_logger_stream_handler(log_level = 'WARNING'):
     return ch
 
 
+class AttribDict(dict):
+    ''' A dictionary with object like attribute access.
+
+    '''
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+        
 
 class Version(object):
     ''' A version String representation.
